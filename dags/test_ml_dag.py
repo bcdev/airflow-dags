eea3b01cd50c20860ce4dc950655017f9f0d726a
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-01-23 16:23:56 +0100

    airflow-dags-ml-test:v1

---
+++ dags/test_ml_dag.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import \
+    KubernetesPodOperator
+
+with DAG(
+    'first_mlflow_dag',
+    schedule_interval=None,
+) as dag:
+    task1 = KubernetesPodOperator(
+        task_id='ml_test_task',
+        name='ml_test-pod',
+        image='syogesh9/airflow-dags-ml-test:v1',
+        cmds=['python', '-m', 'ml_test'],
+        dag=dag,
+        log_events_on_failure=True,
+        get_logs=True,
+    )
---
+++ scripts/ml/ml_test.py
@@ -0,0 +1,9 @@
+import mlflow
+
+mlflow.set_tracking_uri("http://127.0.0.1:5001")
+
+with mlflow.start_run():
+    print("Begin logging")
+    mlflow.log_param("model_type", "logistic_regression")
+    mlflow.log_metric("test_accuracy", 0.95)
+    print("End logging")
