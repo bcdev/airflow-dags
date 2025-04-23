2b0fcfd72340f2867831725b927c779637d46356
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-04-23 16:58:35 +0200

    add dag

---
+++ dags/test_first_dag.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.providers.cncf.kubernetes.operators.pod import \
+    KubernetesPodOperator
+
+with DAG(
+    'first_test_dagv2',
+    schedule_interval=None,
+) as dag:
+    task1 = KubernetesPodOperator(
+        task_id='task1',
+        name='task1-pod',
+        image='syogesh9/airflow-dags-test:v6',
+        cmds=['/opt/conda/bin/python', '-m', 'task1'],
+        dag=dag,
+        log_events_on_failure=True,
+        get_logs=True,
+    )
+
+    task2 = KubernetesPodOperator(
+        task_id='task2',
+        name='task2-pod',
+        image='syogesh9/airflow-dags-test:v6',
+        cmds=['python', '-m', 'task2'],
+        dag=dag,
+        log_events_on_failure=True,
+        get_logs=True,
+    )
+
+    task3 = KubernetesPodOperator(
+        task_id='task3',
+        name='task3-pod',
+        image='syogesh9/airflow-dags-test:v6',
+        cmds=['python', '-m', 'task3'],
+        dag=dag,
+        log_events_on_failure=True,
+        get_logs=True,
+    )
+
+task1 >> task2 >> task3
