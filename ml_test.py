b41aef1be5af35cdc9e061871db0727e334dd0d6
Yogesh Kumar Baljeet Singh <yogesh09121995@gmail.com>
2025-04-23 15:33:48 +0200

    update

---
+++ ml_test.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn.datasets import load_iris
+import pandas as pd
+
+
+X, y = load_iris(return_X_y=True)
+
+model = LogisticRegression(max_iter=200)
+print("Fitting Model!!")
+model.fit(X, y)
+print("Model Fit!!")
+
+sample_data = pd.DataFrame([[5.1, 3.5, 1.4, 0.2]],
+  columns=["sepal length (cm)", "sepal width (cm)", "petal length (cm)", "petal width (cm)"])
+
+prediction = model.predict(sample_data)
+print(f"Prediciton for sample_data {sample_data}: ", prediction)
